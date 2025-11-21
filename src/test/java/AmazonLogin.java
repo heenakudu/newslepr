6f6b28b530ea47dd3abf0b87da48d243ab7597d4
Heena <kuduheena@gmail>
2025-11-22 02:04:43 +0530

    new changes

--- src/test/java/AmazonLogin.java
+++ src/test/java/AmazonLogin.java
@@ -22,7 +22,8 @@ public class AmazonLogin {
         driver.manage().window().maximize();
         driver.get("https://www.amazon.in/-/hi/ap/signin?openid.pape.max_auth_age=3600&openid.return_to=https%3A%2F%2Fwww.amazon.in%2Fspr%2Freturns%2Fgift&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=amzn_psr_desktop_in&openid.mode=checkid_setup&language=en_IN&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0");
         WebElement Username=driver.findElement(By.id("ap_email"));
-        System.out.println("GIT TEST NOW");
+        System.out.println("GIT TEST NOW2");
+
 
         Username.sendKeys("kuduheena@gmail.com");
 WebElement Continue=driver.findElement(By.id("continue"));
